0c19526f90152fde596533367ac6e0e041e20a36
Christiano R Soares <chrisrsoares@gmail.com>
2022-11-04 10:53:34 -0300

    Javascript: Execicios aula 2

--- JavaScript/02 - condicionais/desafio.js
+++ JavaScript/02 - condicionais/desafio.js
@@ -12,3 +12,24 @@ Você terá 5 variáveis. Sendo elas:
 Imprima no console o valor que será gasto de combustível para realizar esta viagem.
 */
 
+const GASOLINA = 'Gasolina';
+const ETANOL = 'Etanol';
+
+const precoEtanol = 4.10;
+const precoGasolina = 4.99;
+const distanciaViagemEmKm = 100;
+const kmPorLitro = 8;
+const tipoCombistovel = ETANOL;
+
+const litrosConsumidos = distanciaViagemEmKm / kmPorLitro;
+
+let previsaoGasto = 0;
+
+if (tipoCombistovel === GASOLINA) {
+    previsaoGasto = litrosConsumidos * precoGasolina;
+} else {
+    previsaoGasto = litrosConsumidos * precoEtanol;
+}
+
+console.log('A previsão de consumo para a viagem de '+ distanciaViagemEmKm + ' Kms utilizando o combustível ' + tipoCombistovel + ' é de R$ '+previsaoGasto.toFixed(2))
+
