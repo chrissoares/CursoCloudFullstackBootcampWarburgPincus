676e6ab5e04fa29c3c42713a534bd3c63b7460d2
Christiano R Soares <chrisrsoares@gmail.com>
2022-11-28 07:32:47 -0300

    Arrays e Estruturas de Repetição em JavaScript: Correção

--- JavaScript/04-ArraysEEstruturasDeRepetiçãoEmJavaScript/02-exercicio_numeros_pares.js
+++ JavaScript/04-ArraysEEstruturasDeRepetiçãoEmJavaScript/02-exercicio_numeros_pares.js
@@ -1,26 +1,24 @@
 //Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número par encontrado
 
 class ListaNumeros{
-    numeroInicial;
-    numeroFinal;
+    lista;
 
-    constructor (inicio, final){
-        this.numeroInicial = inicio;
-        this.numeroFinal = final;
+    constructor (lista){
+        this.lista = lista;
     }
 
     imprimaPares(){
-        console.log(`Números Pares entre ${this.numeroInicial} e ${this.numeroFinal}`);
-        for (let i = this.numeroInicial; i<=this.numeroFinal; i++){
-            if (i % 2 === 0){
-                console.log(`${i}`);
+        console.log(`Números Pares:`);
+        for (let i = 0; i<=this.lista.length; i++){
+            if (this.lista[i] % 2 === 0){
+                console.log(`${this.lista[i]}`);
             }
         }
     }
 }
 
-
 (function (){
-    const lista = new ListaNumeros(10, 55);
+    numeros = [5, 7, 8, 10, 11, 24, 55, 33, 32, 157, 1024, 0];
+    const lista = new ListaNumeros(numeros);
     lista.imprimaPares();
 })()
