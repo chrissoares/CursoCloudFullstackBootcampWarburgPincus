48e91eac425e3fab0ad8daa01571a5d03a7c6b9a
chrissoares <chrisrsoares@gmail.com>
2022-12-29 09:54:08 -0300

    preparando para o desafio

--- JavaScript/06-Pokedex/assets/js/main.js
+++ JavaScript/06-Pokedex/assets/js/main.js
@@ -1,20 +1,21 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
 const limit = 12;
+const maxRecord = 25;
 let offset = 0;
 
-function loadPokemonItens(offset, limit){
+function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
 
         // Primeira forma de usar o MAP, mais didadica
         // const newList = pokemons.map((pokemon) => {
         //     return convertPokemonToLi(pokemon);
         // })
-    
+
         // const newHtml = newList.join('');
-    
+
         // pokemonList.innerHTML += newHtml;
-    
+
         //Outra forma de usar o MAP, para este caso. 
         //Como o MAP recebe chama uma funcao passando como parâmetros
         // Valor, Index e Array, pode referenciar a função diretamente, que ela 
@@ -38,5 +39,15 @@ loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit;
-    loadPokemonItens(offset, limit);
+    const qtdRecordNextPage = offset + limit;
+    if (qtdRecordNextPage >= maxRecord) {
+        const newLimit = maxRecord - offset;
+        loadPokemonItens(offset, newLimit);
+
+        //Chama o pai do loadMoreButton para removê-lo da página.
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+
 })
