2fa6da792fdbfaf22e5f6c0156bf301f7cdc9d82
chrissoares <chrisrsoares@gmail.com>
2023-01-23 08:43:10 -0300

    Finalizado - Trabalhando com Services no Angular

--- WebComponents/10-Service/AngularComSerivce/src/app/components/card/card.component.ts
+++ WebComponents/10-Service/AngularComSerivce/src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  pokemon: PokemonData = { //Outra opção seria inicializar no construtor.
+    id: 0,
+    name: "",
+    sprites: {
+      front_default: ""
+    },
+    types:[]
+  };
 
-  constructor() { }
+  constructor(
+    private service: PokemonService
+  ) { }
 
   ngOnInit(): void {
+    this.getPokemon('bulbasaur');
   }
 
+  getPokemon(searchName: string | number){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next:(res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types,
+          };
+          console.log(this.pokemon);
+          console.log(res);
+        },
+        error: (err) => console.log('Pokemon Not Found'),
+      }
+    )
+  }
 }
